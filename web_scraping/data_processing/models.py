af79e3eebe143850fd5b0c9f326902ce5462c7c4
SzymKam <szymon15kaminski@gmail.com>
2023-10-17 21:05:52 +0200

    feat: add model for word count

--- web_scraping/data_processing/models.py
+++ web_scraping/data_processing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class WordCount(models.Model):
+    article = models.URLField()
+    word = models.CharField(max_length=255)
+    count = models.PositiveIntegerField()
+    author = models.CharField(max_length=255, blank=True)

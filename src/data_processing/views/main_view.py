5f1a0d8bff5f2a7acf41ae09ea6ce7cee32adeeb
SzymKam <szymon15kaminski@gmail.com>
2023-10-20 13:53:58 +0200

    fix: add MainView view, to render main page. Update InitailView

---
+++ src/data_processing/views/main_view.py
@@ -0,0 +1,26 @@
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
+from django.views.generic import View
+
+from ..constans import URL_TO_GET_ARTICLES
+from ..models import WordCount
+from .get_urls import return_urls
+from .get_words import save_words_to_db
+
+
+class InitialView(View):
+    def get(self, request) -> HttpResponse:
+        self.update_db()
+        return redirect("main_view")
+
+    def update_db(self) -> None:
+        urls_with_author = return_urls(base_url=URL_TO_GET_ARTICLES)
+        for article in urls_with_author:
+            save_words_to_db(article=article, urls_with_author=urls_with_author)
+        print("Database updated!")
+
+
+class MainView(View):
+    def get(self, request) -> HttpResponse:
+        authors = WordCount.objects.values("author", "author_lowercase").distinct()
+
+        return render(request, template_name="data_processing/main-template.html", context={"authors": authors})

83b996e9ec1833052df53c7d5ca65fee3a3f2391
SzymKam <szymon15kaminski@gmail.com>
2023-10-21 21:51:42 +0200

    fix: update default variables

--- src/core/default_variables.py
+++ src/core/default_variables.py
@@ -4,8 +4,8 @@ from django.core.management.utils import get_random_secret_key
 env = environ.Env(
     DEBUG=(bool, False),
     SECRET_KEY=(str, get_random_secret_key()),
-    DB_USER=(str, ""),
-    DB_PASSWORD=(str, ""),
-    DB_NAME=(str, ""),
-    DB_HOST=(str, ""),
+    DB_USER=(str, "db_web_scraping"),
+    DB_PASSWORD=(str, "db_web_scraping"),
+    DB_NAME=(str, "db_web_scraping"),
+    DB_HOST=(str, "db"),
 )

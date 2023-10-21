f028b5c57192a27daed0ba6f744fae5ea51e8c72
SzymKam <szymon15kaminski@gmail.com>
2023-10-21 21:59:34 +0200

    fix: update allowed hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
